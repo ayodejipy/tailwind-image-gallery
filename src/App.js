53f645f5d02739bc89ac54b4fbbaf444d5f89c2a
ayodeji_ <ay.programmr@gmail.com>
2020-06-08 10:26:39 +0100

    Fix: CORS error on,

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ImageCards from './components/ImageCards'
 
 function App() {
+    const [ images, setImages ] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ term, setTerm ] = useState('computer');
+
+    useEffect(() => {
+
+      
+      fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`, {
+        method: 'GET',
+        mode: 'cors',
+        redirect: "follow",
+        referrer: "no-referrer",
+        body: null,
+      })
+        .then(response => response.json())
+        .then(data => {
+          setImages(data.hits);
+          setIsLoading(false);
+        })
+        .catch(error => console.log(error));
+    },[]);
+    
   return (
     <div className="container mx-auto">
-      <ImageCards />
+      <div className="grid grid-cols-3 gap-3">
+        {images.map( image => (
+          <ImageCards key={image.id} image={image} />
+        ))}
+      </div>
+      
     </div>
   );
 }
--- src/components/ImageCards.jsx
+++ src/components/ImageCards.jsx
@@ -1,49 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function ImageCards() {
-    const [ Images, setImages ] = useState([]);
-    const [ isLoading, setIsLoading ] = useState(true);
-    const [ term, setTerm ] = useState('');
+const ImageCards = ({ image }) => {  
 
-    useEffect(() => {
-
-    }, []);
-    
+    const tags = image.tags.split(',');
+    // console.log(tags);
 
     return (
         <div className="max-w-sm rounded overflow-lg shadow-lg">
-            <img src="" alt="Johnson" className="w-full" />
+            <img src={image.webformatURL} alt="Johnson" className="w-full" />
             
             <div className="px-6 py-4">
                 <h2 className="font-bold text-purple-500 text-xl mb-3">
-                    Photo by John Doe
+                    {image.user}
                 </h2>
 
                 <ul>
                     <li>
                         <strong>Views: </strong>
-                        4000
+                        {image.views}
                     </li>
                     <li>
                         <strong>Downloads: </strong>
-                        300
+                        {image.downloads}
                     </li>
                     <li>
                         <strong>Likes: </strong>
-                        400
+                        {image.likes}
                     </li>
                 </ul>
             </div>
             <div className="px-6 py-4">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                    #tag1
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                    #tag2
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                    #tag3
-                </span>
+                {tags.map( (tag, index) => (
+                    <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 mt-1 text-sm font-semibold text-gray-700 mr-2">
+                        #{tag}
+                    </span>
+                ))}
+                
             </div>
         </div>
     )
